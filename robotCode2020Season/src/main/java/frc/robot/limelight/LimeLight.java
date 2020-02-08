618edf188436f6393f751647319d27155eddf79a
ewang20027 <37031713+ewang20027@users.noreply.github.com>
2020-02-08 11:50:23 -0800

    Revert "Added distance, pipeline switcher methods. Distance needs to be tested."
    This reverts commit c95563301c3c3756fe919754ccf35393cd1eee88.

--- robotCode2020Season/src/main/java/frc/robot/limelight/LimeLight.java
+++ robotCode2020Season/src/main/java/frc/robot/limelight/LimeLight.java
@@ -6,14 +6,7 @@ import edu.wpi.first.networktables.*;
 
 public final class LimeLight {
     private final String _tableName; 
-	private final NetworkTable _table; 
-	
-	// highest pipeline currently set
-	public static final int maxPipelineIds = 1;
-
-	// adjust as needed
-	private static final double _mountingAngle = 0;
-	private static final double _heightOfCamera = 1; 
+    private final NetworkTable _table; 
 
     /**
      * The LimeLight class constructor. 
@@ -83,53 +76,5 @@ public final class LimeLight {
      */
     public int getCurrentPipelineInt() { 
         return (int)this.getCurrentPipelineDouble();
-	}
-	
-	/**
-	 * Sets the pipeline.
-	 * @param id The pipeline id. 
-	 */
-	public void setPipeline(int id) { 
-		if (id > maxPipelineIds) {
-			id = maxPipelineIds;
-		}
-
-		if (id < 0) {
-			id = 0;
-		}
-
-        NetworkTableEntry pipeline = this._table.getEntry("pipeline");
-		pipeline.forceSetNumber(id);	
-	}
-
-	/**
-	 * Gets the next pipeline. Useful if you want to have a button or controller change the pipeline. 
-	 */
-	public void setNextPipeline() {
-		Number currentPipelineId = this._table.getEntry("pipeline").getNumber(0);	
-		int newPipeline;
-		// if the next number will go past the max registered pipelines
-		// then we want to switch back to 0.
-		if (currentPipelineId.intValue() + 1 > maxPipelineIds) {
-			newPipeline = 0;
-		}
-		else {
-			newPipeline = currentPipelineId.intValue() + 1;
-		}
-		this._table.getEntry("pipeline").forceSetNumber(newPipeline);
-	}
-
-	/**
-	 * Determines the distance from the limelight (or the robot) to the target. 
-	 * @param heightOfTarget The height of the target.  
-	 * @return If a target is found, the distance from the limelight to the target. Otherwise, -1. 
-	 */
-	public double getDistanceFromTarget(double heightOfTarget) { 
-		if (this.hasTarget()) {	
-			return (heightOfTarget - _heightOfCamera) / Math.tan(_mountingAngle + this.getVerticalOffset());
-		}
-		else {
-			return -1; 
-		}
-	}
+    }
 }
