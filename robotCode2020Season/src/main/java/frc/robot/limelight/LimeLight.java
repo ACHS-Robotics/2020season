9fcbac7665a7e57f90810f2827ed91d5e47a9337
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2020-03-01 22:01:26 -0800

    distance?

--- robotCode2020Season/src/main/java/frc/robot/limelight/LimeLight.java
+++ robotCode2020Season/src/main/java/frc/robot/limelight/LimeLight.java
@@ -1,3 +1,4 @@
+
 package frc.robot.limelight;
 
 import edu.wpi.first.networktables.*;
@@ -5,98 +6,102 @@ import edu.wpi.first.networktables.*;
 //import frc.robot.limelight.constants.*;
 
 public final class LimeLight {
-    private final String _tableName; 
-    private final NetworkTable _table; 
-    
-    // highest pipeline currently set
-	public static final int maxPipelineIds = 1;
-
+    private final double _heightOfRobot = 0;
+    private final double _angleOfCamera = 0;
+	private final String _tableName;
+    private final NetworkTable _table;
 
     /**
-     * The LimeLight class constructor. 
+     * The LimeLight class constructor.
      */
-    public LimeLight() { 
+    public LimeLight() {
         this._tableName = "limelight";
         this._table = NetworkTableInstance.getDefault().getTable(this._tableName);
     }
 
     /**
-     * Determines whether the target is found or not. This is represented by the "tv" table in the Shuffleboard. 
-     * @return true if the target is found, false otherwise. 
+     * Determines whether the target is found or not. This is represented by the
+     * "tv" table in the Shuffleboard.
+     * 
+     * @return true if the target is found, false otherwise.
      */
-    public boolean hasTarget() { 
+    public boolean hasTarget() {
         NetworkTableEntry tv = this._table.getEntry("tv");
         return tv.getDouble(0) == 1.0;
     }
 
     /**
-     * Gets the horizontal offset from the crosshair to the target. This can be anywhere from -27 degrees to 27 degrees, inclusive. 
-     * @return The horizontal offset. 
+     * Gets the horizontal offset from the crosshair to the target. This can be
+     * anywhere from -27 degrees to 27 degrees, inclusive.
+     * 
+     * @return The horizontal offset.
      */
-    public double getHorizontalOffset() { 
+    public double getHorizontalOffset() {
         NetworkTableEntry tx = this._table.getEntry("tx");
         return tx.getDouble(0);
     }
-    
+
     /**
-     * Gets the vertical offset from the crosshair to the target. This can be anywhere from -20.5 degrees to 20.5 degrees. 
-     * @return The verical offset. 
+     * Gets the vertical offset from the crosshair to the target. This can be
+     * anywhere from -20.5 degrees to 20.5 degrees.
+     * 
+     * @return The verical offset.
      */
-    public double getVerticalOffset() { 
+    public double getVerticalOffset() {
         NetworkTableEntry ty = this._table.getEntry("ty");
         return ty.getDouble(0);
     }
 
     /**
-     * Gets the target area (0% of image to 100% of image). 
-     * @return The target area (0 to 1, inclusive). 
+     * Gets the target area (0% of image to 100% of image).
+     * 
+     * @return The target area (0 to 1, inclusive).
      */
-    public double getTargetArea() { 
+    public double getTargetArea() {
         NetworkTableEntry ta = this._table.getEntry("ta");
         return ta.getDouble(0);
     }
 
     /**
      * Gets the current camera mode.
+     * 
      * @return The camera mode (either VISION or DRIVER).
      */
-    public String getCurrentCameraMode() { 
+    public String getCurrentCameraMode() {
         NetworkTableEntry camMode = this._table.getEntry("camMode");
-        return LimeLightConstants.getCamModeState((int)camMode.getDouble(0));
+        return LimeLightConstants.getCamModeState((int) camMode.getDouble(0));
     }
 
     /**
      * Returns the current pipeline of the limelight.
-     * @return The current pipeline. 
+     * 
+     * @return The current pipeline.
      */
-    public double getCurrentPipelineDouble() { 
+    public double getCurrentPipelineDouble() {
         NetworkTableEntry pipeline = this._table.getEntry("pipeline");
         return pipeline.getDouble(0.0);
     }
 
     /**
      * Returns the current pipeline of the limelight.
-     * @return The current pipeline. 
+     * 
+     * @return The current pipeline.
      */
-    public int getCurrentPipelineInt() { 
-        return (int)this.getCurrentPipelineDouble();
+    public int getCurrentPipelineInt() {
+        return (int) this.getCurrentPipelineDouble();
     }
 
-    	/**
-	 * Sets the pipeline.
-	 * @param id The pipeline id. 
-	 */
-	public void setPipeline(int id) { 
-		if (id > maxPipelineIds) {
-			id = maxPipelineIds;
-		}
-
-		if (id < 0) {
-			id = 0;
-		}
-
-        NetworkTableEntry pipeline = this._table.getEntry("pipeline");
-		pipeline.forceSetNumber(id);	
+    /**
+     * Returns the distance from the camera to the target.
+     * 
+     * @return The distance, if it exists; otherwise, -1.
+     */
+    public double getDistance(double heightOfTarget) {
+        if (!this.hasTarget()) {
+            return -1;
+        }
+        double heightDiff = heightOfTarget - this._heightOfRobot;
+        double camAndTargetAngle = Math.toRadians(this._angleOfCamera + this.getVerticalOffset());
+        return heightDiff / Math.tan(camAndTargetAngle);
     }
-    
 }
